50310f5b05da1f6c444e341481a3a98467d18c77
Keewon Shin <kevinkwshin@gmail.com>
2019-09-05 08:43:32 +0900

    Update losses.py

--- losses.py
+++ losses.py
@@ -32,11 +32,11 @@ def Active_Contour_Loss(y_true, y_pred):
     loss =  lenth + lambdaP * (region_in + region_out) 
     """
     #	lenth term
-    x = y_pred[:,:,1:,:] - y_pred[:,:,:-1,:] # horizontal and vertical directions 
-    y = y_pred[:,:,:,1:] - y_pred[:,:,:,:-1]
+    x = y_pred[:,:,1:,:] - y_pred[:,:,:-1,:] 
+    y = y_pred[:,1:,:,:] - y_pred[:,:-1,:,:]
+    delta_x = x[:,:-2,1:,:]**2
+    delta_y = y[:,1:,:-2,:]**2
 
-    delta_x = x[:,:,1:,:-2]**2
-    delta_y = y[:,:,:-2,1:]**2
     delta_u = K.abs(delta_x + delta_y) 
 
     epsilon = 0.00000001 # where is a parameter to avoid square root is zero in practice.
@@ -47,8 +47,7 @@ def Active_Contour_Loss(y_true, y_pred):
     region_in = K.abs(K.sum(y_pred * ((y_true - 1) ** 2)))
     region_out = K.abs(K.sum( (1-y_pred) * ((y_true)**2) )) # equ.(12) in the paper
 
-    # summary  
+    # finally
     lambdaP = 1 # lambda parameter could be various.
     loss =  lenth + lambdaP * (region_in + region_out) 
-
     return loss
