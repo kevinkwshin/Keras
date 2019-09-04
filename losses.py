56005a71da7bf28c09dfa0a7e9e4eb0d7bd64e48
Keewon Shin <kevinkwshin@gmail.com>
2019-09-05 00:08:24 +0900

    Update losses.py

--- losses.py
+++ losses.py
@@ -27,28 +27,28 @@ import numpy as np
 
 def Active_Contour_Loss(y_true, y_pred): 
 
-	"""
-  https://github.com/xuuuuuuchen/Active-Contour-Loss
-	loss =  lenth + lambdaP * (region_in + region_out) 
-	"""
-  #	lenth term
-	x = y_pred[:,:,1:,:] - y_pred[:,:,:-1,:] # horizontal and vertical directions 
-	y = y_pred[:,:,:,1:] - y_pred[:,:,:,:-1]
+    """
+    https://github.com/xuuuuuuchen/Active-Contour-Loss
+    loss =  lenth + lambdaP * (region_in + region_out) 
+    """
+    #	lenth term
+    x = y_pred[:,:,1:,:] - y_pred[:,:,:-1,:] # horizontal and vertical directions 
+    y = y_pred[:,:,:,1:] - y_pred[:,:,:,:-1]
 
-	delta_x = x[:,:,1:,:-2]**2
-	delta_y = y[:,:,:-2,1:]**2
-	delta_u = K.abs(delta_x + delta_y) 
+    delta_x = x[:,:,1:,:-2]**2
+    delta_y = y[:,:,:-2,1:]**2
+    delta_u = K.abs(delta_x + delta_y) 
 
-	epsilon = 0.00000001 # where is a parameter to avoid square root is zero in practice.
-	w = 1
-	lenth = w * K.sum(K.sqrt(delta_u + epsilon)) # equ.(11) in the paper
+    epsilon = 0.00000001 # where is a parameter to avoid square root is zero in practice.
+    w = 1
+    lenth = w * K.sum(K.sqrt(delta_u + epsilon)) # equ.(11) in the paper
 
-	# region term
-  region_in = K.abs(K.sum(y_pred * ((y_true - 1) ** 2)))
-  region_out = K.abs(K.sum( (1-y_pred) * ((y_true)**2) )) # equ.(12) in the paper
+    # region term
+    region_in = K.abs(K.sum(y_pred * ((y_true - 1) ** 2)))
+    region_out = K.abs(K.sum( (1-y_pred) * ((y_true)**2) )) # equ.(12) in the paper
 
-  # summary  
-	lambdaP = 1 # lambda parameter could be various.
-	loss =  lenth + lambdaP * (region_in + region_out) 
+    # summary  
+    lambdaP = 1 # lambda parameter could be various.
+    loss =  lenth + lambdaP * (region_in + region_out) 
 
-	return loss
+    return loss
