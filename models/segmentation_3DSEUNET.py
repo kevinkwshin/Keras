59cc94f043d0ccc18b3465e6535fa9d6508c51d6
Keewon Shin <kevinkwshin@gmail.com>
2019-09-25 10:43:35 +0900

    Update segmentation_3DSEUNET.py

--- models/segmentation_3DSEUNET.py
+++ models/segmentation_3DSEUNET.py
@@ -4,7 +4,7 @@ from tensorflow.keras.utils import to_categorical
 
 from tensorflow.keras import layers, initializers, regularizers, constraints
 from tensorflow.keras.layers import *
-from tensorflow import keras as K
+from tensorflow.keras import backend as K
 
 def load_model(input_shape, num_labels, axis=-1, base_filter=32, depth_size=4, se_res_block=True, se_ratio=16, noise=0.2, last_relu=False, atten_gate=False):
     
