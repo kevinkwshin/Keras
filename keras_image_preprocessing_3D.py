a0ec8f36eb5933ad6b836b6a8c9d877c74ba44b7
Keewon Shin <kevinkwshin@gmail.com>
2020-05-26 16:17:15 +0900

    Update keras_image_preprocessing_3D.py

--- keras_image_preprocessing_3D.py
+++ keras_image_preprocessing_3D.py
@@ -18,8 +18,7 @@ from tensorflow.keras import backend as K
 # import keras
 
 
-def random_rotation(x, rg, row_index=2, col_index=3, dep_index = 1, channel_index=0,
-                    fill_mode='nearest', cval=0.):
+def random_rotation(x, rg, row_index=2, col_index=3, dep_index = 1, channel_index=0, fill_mode='nearest', cval=0.):
     theta1 = np.pi / 180 * np.random.uniform(-rg[0], rg[0])
     theta2 = np.pi / 180 * np.random.uniform(-rg[1], rg[1])
     theta3 = np.pi / 180 * np.random.uniform(-rg[2], rg[2])
@@ -29,13 +28,13 @@ def random_rotation(x, rg, row_index=2, col_index=3, dep_index = 1, channel_inde
                                   [0, 0, 1, 0],
                                   [0, 0, 0, 1]])
     rotation_matrix_y = np.array([[np.cos(theta2), 0, -np.sin(theta2), 0],
-                                              [0, 1, 0, 0],
-                                              [np.sin(theta2), 0, np.cos(theta2), 0],
-                                              [0, 0, 0, 1]])
-    rotation_matrix_y = np.array([[1, 0, 0, 0],
-                                              [0, 1, 0, 0],
-                                              [0, 0, 1, 0],
-                                              [0, 0, 0, 1]])
+                                  [0, 1, 0, 0],
+                                  [np.sin(theta2), 0, np.cos(theta2), 0],
+                                  [0, 0, 0, 1]])
+#     rotation_matrix_y = np.array([[1, 0, 0, 0],
+#                                   [0, 1, 0, 0],
+#                                   [0, 0, 1, 0],
+#                                   [0, 0, 0, 1]])
     rotation_matrix_x = np.array([[1, 0, 0, 0],
     	                          [0, np.cos(theta3), -np.sin(theta3), 0],
                                   [0, np.sin(theta3), np.cos(theta3), 0],
